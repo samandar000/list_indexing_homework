4957039b6babe1887a9b8025aeca60c04d40eab6
Samandar000 <samandarnfh.2020@gmail.com>
2022-08-25 16:41:13 +0500

    07

--- List07.py
+++ List07.py
@@ -6,4 +6,11 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    i=0
+    while i < len(list1):
+        if list1[i]==0:
+            list1[i]=False
+        i+=1
+        
+    return list1
+print(main([1, 0, 0, 0, 0]))
